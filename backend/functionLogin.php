0266191a618f43349d39f75afc06fc2e2660f2c2
RhuanAz <0063404@academico.ifmg.edu.br>
2023-09-15 14:25:20 -0300

    Login funcionando

--- backend/functionLogin.php
+++ backend/functionLogin.php
@@ -11,9 +11,39 @@ if (empty($_POST['email']) || empty($_POST['senha'])) {
 $email = mysqli_real_escape_string($conn, $_POST['email']);
 $senha = mysqli_real_escape_string($conn, $_POST['senha']);
 
-$sql = "SELECT * FROM cliente WHERE (email = '".$email ."') AND (senha = '". $senha. "')";
+$sql = "SELECT * FROM cliente WHERE (email = '".$email ."') LIMIT 1";
+$sql_exec = $conn->query($sql);
 
-$result = mysqli_query($conn, $sql);
+$usuario = $sql_exec->fetch_assoc();
+
+if(password_verify($senha, $usuario['senha'])){
+    echo "Usuário Logado";
+    $_SESSION['email'] = $email;
+    header('Location: ../list.php');
+    exit();
+}else{
+    echo "Falha ao logar";
+    $_SESSION['nao_autenticado'] = true;
+    header('Location: ../login.php');
+    exit();
+}
+
+/*
+$result = mysqli_query($conection, $query);
+
+$row = mysqli_num_rows($result);
+
+if ($row == 1) {
+    $_SESSION['email'] = $email;
+    header('Location: ../list.php');
+    exit();
+} else {
+    $_SESSION['nao_autenticado'] = true;
+    header('Location: ../login.php');
+    exit();
+}
+
+
 
 $row = mysqli_num_rows($result);
 
@@ -26,4 +56,4 @@ if ($row == 1) {
     $_SESSION['nao_autenticado'] = true;
     header('Location: ../login.php');
     exit();
-}
+}*/
